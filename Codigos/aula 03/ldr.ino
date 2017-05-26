b51eaa6e0600c6aa7191ef17871aa66f59358cc2
Abraão Állysson dos Santos Honório <abraao.allyson@eng.ci.ufpb.br>
2017-05-26 11:50:44 -0300

    código do LDR - encontro 03

---
+++ Codigos/aula 03/ldr.ino
@@ -0,0 +1,20 @@
+
+        private Nullable<int> id;
+
+        private String name;
+        private String password;
+
+        private String email;
+        private String type;
+        private String cpnjCpf;
+
+        private String phone;
+        private String numberCard;
+
+        private String cardPassword;
+        private String cvv;
+        private String status;
+    
+        private String city;
+        private String state;
+        private String zipCode;
