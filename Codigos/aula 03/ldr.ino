bc5e987a8c7e9f441712398d0a04582334589c95
Abraão Állysson dos Santos Honório <abraao.allyson@eng.ci.ufpb.br>
2017-05-26 11:52:00 -0300

    Update ldr.ino

--- Codigos/aula 03/ldr.ino
+++ Codigos/aula 03/ldr.ino
@@ -1,20 +1,37 @@
 
-        private Nullable<int> id;
 
-        private String name;
-        private String password;
+int ledPinSeis = 6; //Led no pino 6 
+int ledPinSete = 7; //Led no pino 7
+int ldrPin = 0; //LDR no pino analígico 8
+int ldrValor = 0; //Valor lido do LDR
+ 
+//Declaração de Variáveis *globais*.
+void setup() 
+{
+   pinMode(ledPinSeis,OUTPUT); //define a porta 6 como saída
+   pinMode(ledPinSete,OUTPUT); //define a porta 7 como saída
+   Serial.begin(9600); //Inicia a comunicação serial
+}
+ 
+void loop()
+{
+ ldrValor = analogRead(ldrPin);  ///ler o valor do LDR, vale lembrar que o valor lido será entre 0 e 1023
+ 
+//Se o valor lido for maior que 600(valor ajustável), liga o led pin 7 e apaga o ledpin 6
+if (ldrValor>= 600)
+{
+  digitalWrite(ledPinSete,HIGH);
+  digitalWrite(ledPinSeis,LOW);
+} 
+ //Senão, apaga o apaga o led pin 7 e liga o ledpin 6
+else
+{
+  digitalWrite(ledPinSete,LOW);
+  digitalWrite(ledPinSeis,HIGH);
+} 
+ 
+ Serial.println(ldrValor); //imprime o valor LDR(ldrValor) no monitor serial
 
-        private String email;
-        private String type;
-        private String cpnjCpf;
-
-        private String phone;
-        private String numberCard;
-
-        private String cardPassword;
-        private String cvv;
-        private String status;
-    
-        private String city;
-        private String state;
-        private String zipCode;
+ delay(100); //dealy em milissegundos
+ 
+}
